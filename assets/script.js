f52b6a22f9370e10042ba1e2ffadcabefcd612a4
Jax I <jaxpi85@gmail.com>
2022-12-28 11:48:05 -0500

    trying to make a PWA

--- assets/script.js
+++ assets/script.js
@@ -143,3 +143,12 @@ formEl.on('submit', handleFormSubmit);
 cityListEl.on("click", function(event){
   handleFormSubmit(event);
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
